006fe32ec3a913c7beec12c1935c04e71f92a5e6
Яковлев Андрей Михайлович <andrei.yakauleu@softclub.by>
2023-10-20 09:38:55 +0300

    Dropped afield (corrected a mistake)

--- output_local.sql
+++ output_local.sql
@@ -1,8 +1,8 @@
 
 UPDATE SUMMARY
-Run:                          1
+Run:                          2
 Previously run:               7
 Filtered out:                 0
 -------------------------------
-Total change sets:            8
+Total change sets:            9
 
